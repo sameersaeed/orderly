4273bccf867355f236e4ba3543b7933ca203a705
Sameer <sameer.saeed@ibm.com>
2024-06-21 19:54:56 -0400

    add price formatting and order id + filter by user id, in order history

--- client/src/components/Orders.tsx
+++ client/src/components/Orders.tsx
@@ -2,10 +2,11 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 interface Order {
-    id: string;
-    userId: string;
     cart: Array<{ item: string, quantity: number, price: number }>;
+    email: string;
+    orderID: string
     totalPrice: number;
+    userID: string;
 }
 
 const Orders: React.FC<{ user: any }> = ({ user }) => {
@@ -17,7 +18,8 @@ const Orders: React.FC<{ user: any }> = ({ user }) => {
                 const response = await axios.get<Order[]>(`${process.env.REACT_APP_HOST_URL}:${process.env.REACT_APP_SERVER_PORT}/getOrders`, {
                     headers: { Authorization: `Bearer ${user.Token}` }
                 });
-                setOrders(response.data);
+                setOrders(response.data.filter(order => order.userID === user.id));
+                ;
             } catch (error) {
                 console.error('ERROR: could not fetch orders:', error);
             }
@@ -33,12 +35,12 @@ const Orders: React.FC<{ user: any }> = ({ user }) => {
             <h1>Orders</h1>
             <div className="row row-cols-1 row-cols-md-2 g-4">
                 {orders?.map((order: Order) => (
-                    <div key={order.id} className="col">
+                    <div key={order.orderID} className="col">
                         <div className="card bg-dark text-light">
                             <div className="card-body">
-                                <h5 className="card-title">Order ID: {order.id}</h5>
+                                <h5 className="card-title">Order ID: {order.orderID}</h5>
                                 <p className="card-text">Items: {order.cart.map(item => `${item.item} (x${item.quantity})`).join(', ')}</p>
-                                <p className="card-text">Total: ${order.totalPrice}</p>
+                                <p className="card-text">Total: ${order.totalPrice.toFixed(2)}</p>
                             </div>
                         </div>
                     </div>
